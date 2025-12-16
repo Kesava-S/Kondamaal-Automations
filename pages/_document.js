94428000057a3670dfbe4aff5f5fed0c9f76001d
Kesavaram S <kesav@Mac.communityfibre.co.uk>
2025-12-16 12:26:47 +0000

    Add properly sized favicons (48x48, 192x192, 512x512) for Google Search visibility

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,10 @@ export default function Document() {
         {/* Preconnect to critical domains */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="icon" href="/logo.png" type="image/png" sizes="any" />
-        <link rel="icon" href="/logo.png" type="image/png" sizes="32x32" />
-        <link rel="apple-touch-icon" href="/logo.png" />
-        <link rel="shortcut icon" href="/logo.png" type="image/png" />
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="48x48" />
+        <link rel="icon" href="/icon-192.png" type="image/png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
 
         {/* Critical CSS for LCP elements (Hero) */}
         <style dangerouslySetInnerHTML={{
